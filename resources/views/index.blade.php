73a1f6bf50ecc9612661aa0a57569137b5b9e065
ABR-Adorsho <ashab.bin.rayhan@gmail.com>
2022-12-24 02:37:33 +0600

    Update Form Design with tailwind CSS and removed unnecessary files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,21 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.5/dist/flowbite.min.css" />
 
     <title>Flight Search</title>
 
-    <style>
-        /* custom styles */
-        body {
-            padding: 4em;
-            padding-top: 5px;
-            background: #ffff;
-            font: 13px/1.4 Geneva, 'Lucida Sans', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
-        }
+    @vite('resources/css/app.css')
 
+    <style>
         #tbstyle {
             font-family: Arial, Helvetica, sans-serif;
             border-collapse: collapse;
@@ -38,187 +32,172 @@
         #tbstyle th {
             padding-top: 12px;
             padding-bottom: 12px;
-            /*text-align: center;*/
             background-color: rgba(140, 158, 204, 0.66);
 
         }
-
         #tbstyle td{
             border-bottom: 1px solid #cb2323;
         }
-        .button {
-            background-color: #1D3EC7FF; /* Blue */
-            border: none;
-            -webkit-border-radius: 4px;
-            -moz-border-radius: 4px;
-            border-radius: 4px;
-            color: white;
-            padding: 3px 15px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-        }
-        .button:hover {background-color: rgba(15, 15, 16, 0.48)
-        }
-
-        .form-inline {
-            display: flex;
-            flex-flow: row wrap;
-            align-items: center;
-        }
-
-        .form-inline label {
-            margin: 5px 10px 5px 0;
-        }
-
-        .form-inline input {
-            vertical-align: middle;
-            margin: 5px 10px 5px 0;
-            padding: 10px;
-            background-color: #fff;
-            border: 1px solid #ddd;
-        }
-
-        .form-inline select {
-            vertical-align: middle;
-            margin: 5px 10px 5px 0;
-            /*padding: 10px;*/
-            background-color: #fff;
-            border: 1px solid #ddd;
-        }
-
-        .form-inline button {
-            padding: 10px 20px;
-            background-color: dodgerblue;
-            border: 1px solid #ddd;
-            color: white;
-            cursor: pointer;
-        }
-
-        .form-inline button:hover {
-            background-color: royalblue;
-        }
-        [type="date"] {
-            background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
-        }
-        [type="date"]::-webkit-inner-spin-button {
-            display: none;
-        }
-        [type="date"]::-webkit-calendar-picker-indicator {
-            opacity: 0;
-        }
-
-        label {
-            display: block;
-        }
-        .input {
-            border: 1px solid #c4c4c4;
-            border-radius: 5px;
-            background-color: #fff;
-            padding: 3px 5px;
-            box-shadow: inset 0 3px 6px rgba(0,0,0,0.1);
-            width: 190px;
-        }
-
-        .bd-example>:last-child {
-            margin-bottom: 0;
-        }
-        .form-select {
-            display: inline-block;
-            height: calc(1.5em + .75rem + 2px);
-            line-height: 1.5;
-            border: 1px solid #c4c4c4;
-            border-radius: 5px;
-            background-color: #fff;
-            padding: 3px 5px;
-            box-shadow: inset 0 3px 6px rgba(0,0,0,0.1);
-            width: 106px;
-            color: #495057;
-            vertical-align: middle;
-            background: #fff url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='4' height='5' viewBox='0 0 4 5'%3e%3cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3e%3c/svg%3e") right .75rem center/8px 10px no-repeat;
-            -webkit-appearance: none;
-            -moz-appearance: none;
-            appearance: none;
-        }
-        @media (max-width: 350px) {
-            .form-inline input {
-                margin: 10px 0;
-            }
-
-            .form-inline {
-                flex-direction: column;
-                align-items: stretch;
-            }
-        }
     </style>
+
 </head>
 <body>
-<h2 style="text-align: center">Master Price</h2>
+<h2 class="my-6 text-2xl font-bold" style="text-align: center;">Master Price</h2>
 <hr style="border: 1px solid rgba(93,97,107,0.29);">
-<div class="container" style="width:100%;">
-    <div class="text-center mb-3">
 
-        <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="option2">Round Trip</label>
 
-        <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
-        <label class="btn btn-outline-primary" for="option1">One Way</label>
+<div class="container mx-auto  px-4">
+    <style>
+        li a {
+            text-decoration: none;
+            color:#888;
+        }
 
-        <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
-        <label class="btn btn-outline-primary" for="option4">Multi City</label>
+        .segmented-control {
+            display: inline-block;
+            border: 1px solid #1d3ec7;
+            border-radius: 8px;
+            overflow: hidden;
+        }
+        .segmented-control__item {
+            float: left;
+            list-style-type: none;
+        }
+        .segmented-control__input {
+            position: absolute;
+            visibility: hidden;
+        }
+        .segmented-control__label {
+            display: block;
+            padding: 1em 4em;
+            margin: -1px 0;
+            border-left: 1px solid #ddd;
+            text-align: center;
+            cursor: pointer;
+        }
+        .segmented-control__item:first-child .segmented-control__label {
+            border-left-width: 0;
+        }
+        .segmented-control__label:hover {
+            background: #fafafa;
+        }
+        .segmented-control__input:checked + .segmented-control__label {
+            background: #1d3ec7;
+            color: #ffffff;
+            font-weight: 600;
+        }
+    </style>
+
+    <div>
+        <ul class="segmented-control my-2">
+            <li class="segmented-control__item">
+                <a href="#"><input class="segmented-control__input" type="radio" value="1" name="option" id="option-1">
+                    <label class="segmented-control__label" for="option-1">Round Trip</label></a>
+            </li>
+            <li class="segmented-control__item">
+                <a href="#"><input class="segmented-control__input" type="radio" value="2" name="option" id="option-2" checked >
+                    <label class="segmented-control__label" for="option-2">One Way</label></a>
+            </li>
+            <li class="segmented-control__item">
+                <a href="#"><input class="segmented-control__input" type="radio" value="3" name="option" id="option-3" >
+                    <label class="segmented-control__label" for="option-3">Multi City</label></a>
+            </li>
+        </ul>
     </div>
-    <div class="form-container">
-        <form class="form-inline" id="form1" action="{{route('flight-info')}}">
-            @csrf
-            <input type="text" class="input" id="departure" placeholder="Departure" name="departure">
-            <input type="text" class="input" id="arrival" placeholder="Arrival" name="arrival">
-            <input type="date" class="input" name="dateofbirth">
-            <select class="form-select">
-                <option selected>Day-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <select class="form-select">
-                <option selected>Day+</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <select class="form-select">
-                <option selected>Time</option>
-                <option value="Anytime">Anytime</option>
-                <option value="Midnight">Midnight</option>
-                <option value="Morning">Morning</option>
-            </select>
-            <select class="form-select">
-                <option selected>Type</option>
-                <option value="ADT">ADT</option>
-                <option value="CHLD">CHLD</option>
-            </select>
-            <select class="form-select">
-                <option selected>Number</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-            <br>
-            <hr style="border: 1px solid rgba(93,97,107,0.29);">
-        </form>
+
+    <hr style="border: 1px solid rgba(93,97,107,0.29);">
+
+    <form class="max-w-full my-2 py-4" id="form1" action="{{route('flight-info')}}">
+        @csrf
+        <div class="flex flex-wrap -mx-2 mb-2">
+            <div class="w-full md:w-fit px-1 mb-4">
+                <input class="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="From">
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <input class="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="To">
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4">
+                <div class="relative">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <input datepicker datepicker-autohide type="text" class="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-9 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Select date">
+                </div>
+
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <div class="relative">
+                    <select class="block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option selected>Day-</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                </div>
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <div class="relative">
+                    <select class="block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option selected>Day+</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+
+                </div>
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <div class="relative">
+                    <select class="block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option selected>Time</option>
+                        <option value="Anytime">Anytime</option>
+                        <option value="Midnight">Midnight</option>
+                        <option value="Morning">Morning</option>
+                    </select>
+
+                </div>
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <div class="relative">
+                    <select class="block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option selected>Type</option>
+                        <option value="ADT">ADT</option>
+                        <option value="CHLD">CHLD</option>
+                    </select>
+
+                </div>
+            </div>
+            <div class="w-full md:w-fit px-1 mb-4 ">
+                <div class="relative">
+                    <select class="block appearance-none w-full  border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option selected>Person</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+
+                </div>
+            </div>
+        </div>
+    </form>
+    <form id="form2" area-hidden="true" style="display: none; margin: 0; padding: 0;" action="{{route('home')}}"></form>
+    <hr style="border: 1px solid rgba(93,97,107,0.29);" class="mb-2">
+    <div class="grid grid-cols-6 gap-4">
+        <div class="col-end-8 col-span-1 ...">
+            <button type="submit" form="form2" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                Reset
+            </button>
+        </div>
+        <div class="col-end-10 col-span-2 ...">
+            <button type="submit" form="form1" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+        </div>
     </div>
     <hr style="border: 1px solid rgba(93,97,107,0.29);">
-    <div style="width: 100%; display: flex; justify-content: space-between">
-        <button style="margin-right: 5px;"><a style="text-decoration: none; color: #000;" href="{{route('home')}}">Reset</a></button>
-        <button  style=" " class="button" form="form1" type="submit">Search</button>
-    </div>
-    <hr style="border: 1px solid rgba(93,97,107,0.29);">
-    <div class="msg-container">
+
+    <div class="msg-container mt-2">
         @if(!(empty($data)))
             {{ $data['message'] }}
         @endif
@@ -296,7 +275,7 @@
                             @endforeach
                         </td>
                         <td>
-                            {{$flightOffer['price']}} <br> <button class="button" id="btn1">Select</button>
+                            {{$flightOffer['price']}} <br> <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Select</button>
                         </td>
                     </tr>
                 @endforeach
@@ -306,6 +285,9 @@
         </table>
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://unpkg.com/flowbite@1.5.5/dist/datepicker.js"></script>
+<script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+
+//root route
 Route::get('/', function () {
-
-    $flightInfo = file_get_contents(base_path('/public/LHR_CDG_ADT1_TYPE_1.json'));
-
-    $data = json_decode($flightInfo, true);
-
-
     return view('index');
-
 })->name('home');
 
+//route to fetch data from json file
 Route::get('/get-flight-info/', function () {
     $flightInfo = file_get_contents(base_path('/public/LHR_CDG_ADT1_TYPE_1.json'));
 
